d40624f4bb3e1478e2afcffcfcc7cac872d882c7
Alexander Kimani <a906k620@ku.edu>
2016-03-23 18:33:58 -0500

    Fix addition of empty string CreateUser.php

--- CreateUser.php
+++ CreateUser.php
@@ -7,18 +7,22 @@ if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-
-if ($result = $mysqli->query("insert into Users(user_id) values('$user')"))
+if(strlen($user) > 0)
 {
-    printf("Successfully added user ".$user."<br>");
-    $result->close();
+    if ($result = $mysqli->query("insert into Users(user_id) values('$user')"))
+    {
+        printf("Successfully added user ".$user."<br>");
+        $result->close();
+    }
+    else
+    {
+        printf("Failed to add user ".$user."<br>");
+    }
+
 }
 else
 {
-     printf("Failed to add user ".$user."<br>");
+    printf("Failed to add user ".$user."<br>");
 }
-
-
-
 
 ?>
