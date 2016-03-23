1398bdd3c13ce01b701da869a36040127159a6fb
Alexander Kimani <a906k620@ku.edu>
2016-03-23 18:06:12 -0500

    Initial commit CreateUser.php

---
+++ CreateUser.php
@@ -0,0 +1,24 @@
+<?php
+$user = $_POST["user"];
+$mysqli = new mysqli("mysql.eecs.ku.edu", "akimani", "1Guthondeka.", "akimani");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+if ($result = $mysqli->query("insert into Users(user_id) values('$user')"))
+{
+    printf("Successfully added user ".$user."<br>");
+    $result->close();
+}
+else
+{
+     printf("Failed to add user ".$user."<br>");
+}
+
+
+
+
+?>
