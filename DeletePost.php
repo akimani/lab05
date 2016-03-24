31ba3d7e124293bc6758c8ad0240af9f910afb0d
Alexander Kimani <a906k620@ku.edu>
2016-03-24 13:52:11 -0500

    Delete and show a list of deleted post_ids

--- DeletePost.php
+++ DeletePost.php
@@ -2,7 +2,9 @@
 $checked = $_POST["check"];
 
 $todelete = implode("','",$checked);
-echo$todelete;
+$arrSize = count($checked);
+
+
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "akimani", "1Guthondeka.", "akimani");
 /* check connection */
@@ -13,7 +15,9 @@ if ($mysqli->connect_errno) {
 
 if($result = $mysqli->query("delete from Posts where post_id in ('$todelete')"))
 {
-    printf("deleted the posts");
+    printf("deleted posts ");
+    echo $todelete;
+
 }
 
 
